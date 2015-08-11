a810716b341faf76f41ec7b83229900ff8186b46
ironcircle <zxjszy@126.com>
2015-08-11 18:05:21 +0800

    H.cpp added

--- training/2015.8.11/H.cpp
+++ training/2015.8.11/H.cpp
@@ -1,51 +1,29 @@
 #include<iostream>
 #include<cstdio>
-#include<map>
-#include<set>
 using namespace std;
 
-int n, m, t, step;
-long long tot_ans;
-int g[3000000],f[3000000];
-int min2(int p, int q){
+int n, m, t;
+
+long long min(const long long &p, const long long &q){
 	if (p == -1) return q;
 	if (q == -1) return p;
-	return p < q ? p : q;
+	return p > q ? q : p;
 }
-
-int ttry(int p, int q, int r){
-	int s = p / r;
-	int remain = p % r;
-
-
-}
-
-
-void make(int p, int q){
-	if (p == q)
-		return;
-	for (int i = p; i <= q; i++)
-		g[x] = f[x];
-	int x = (q - p) / 2;
-	for (int i = 1; i <= x; i++)
-	{
-		f[i + p] = g[i * 2 - 1 + p];
-		f[i + x + p] = g[i * 2 + p];
-	}
-	make(p, x);
-	make(x + 1, q);
+long long find(long long p, long long q){
+	long long ans = -1;
+	if (p == 0) return q - 1;
+	if (p < q) return ans;
+	ans = min(ans, find(p, q * 2));
+	ans = min(ans, find(p, q * 2 - 1));
+	ans = min(ans, find(p - q, q * 2));
+	ans = min(ans, find(p - q - 1, q * 2 + 1));
+	return ans;
 }
 int main(){
-	for (int i = 1; i <= (1 << 20); i++)
-		f[i] = i;
-	make(1, 1 << 20);
 	while (scanf("%d%d", &n, &m) == 2){
 		t = m - n + 1;
-		if (n == 0) printf("%d\n", m); else
-		{
-			for (int i = 1; i <= n / max((t - 1), 1); i++)
-				ans = min2(ans, ttry(n, t, i));
-			printf("%d\n", ans);
-		}
+		if (t==1) cout<<m<<endl;else cout << find(n, t) << endl;
 	}
 }
+
+
